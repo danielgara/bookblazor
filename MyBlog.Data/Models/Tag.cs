5816e0558c2cae0e559a8b9591c5fd357136cca2
Daniel Correa <yo@danielgara.com>
2021-07-06 13:55:18 -0500

    first commit

---
+++ MyBlog.Data/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MyBlog.Data.Interfaces;
+namespace MyBlog.Data.Models
+{
+    public class Tag : IMyBlogItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<BlogPost> BlogPosts
+        {
+            get;
+            set;
+        }
+    }
+}
