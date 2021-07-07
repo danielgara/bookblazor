a9a8b139ac6a7ab1003bbe8552991d5f1fe3f11d
Daniel Correa <yo@danielgara.com>
2021-07-07 16:13:35 -0500

    imp: chapter 06 completed

--- MyBlog.Data/Models/BlogPost.cs
+++ MyBlog.Data/Models/BlogPost.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using MyBlog.Data.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
 namespace MyBlog.Data.Models
 {
     public class BlogPost : IMyBlogItem
     {
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
         public string Title { get; set; }
+
+        [Required]
         public string Text { get; set; }
+
         public DateTime PublishDate { get; set; }
+
         public Category Category { get; set; }
+
         public ICollection<Tag> Tags { get; set; }
     }
 }
--- MyBlog.Data/Models/Category.cs
+++ MyBlog.Data/Models/Category.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
 using MyBlog.Data.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
 namespace MyBlog.Data.Models
 {
     public class Category : IMyBlogItem
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public ICollection<BlogPost> BlogPosts
         {
             get;
